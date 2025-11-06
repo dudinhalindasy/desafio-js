092eaece1714168957b3d5741099eb02d45ac60b
dudinhalindasy <eduarda.missel.gomes@escola.pr.gov.br>
2025-11-05 21:11:36 -0300

    Add files via upload

---
+++ desafio.js
@@ -0,0 +1,9 @@
+console.log("Meu primeiro programa: trabalhando com variaveis")
+const idade = 16;
+console.log("idade");
+console.log(idade);
+console.log(idade + 2);
+console.log(idade - 2);
+console.log(idade / 2);
+const idadeSomada = idade + 2;
+console.log(idadeSomada);
